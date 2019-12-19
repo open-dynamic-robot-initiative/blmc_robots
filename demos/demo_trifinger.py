abe0d5df68a9a3c82e6e3e5ec9ce9ace3a229a6a
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2019-12-19 12:59:08 +0100

    WIP extend and refactor trifinger dmeo

--- demos/demo_trifinger.py
+++ demos/demo_trifinger.py
@@ -13,6 +13,71 @@ import blmc_robots
 
 N_JOINTS = 9
 
+def run_experimental(robot):
+    """Move the legs in some hard-coded choreography."""
+
+    def perform_step(position):
+        t = robot.frontend.append_desired_action(robot.Action(position=position))
+        time.sleep(1)
+
+    deg45 = np.pi / 4
+    deg90 = np.pi / 2
+
+    finger0 = [-deg45, -deg45 - np.pi/6, -deg90 - np.pi/6]
+    finger120_open = [-deg90, 0, +deg45]
+    finger120_clash = [deg45, 0, -deg45/2]
+    finger240_open = [-deg45, 0, -deg90]
+    finger240_clash = [deg45, 0, 0]
+
+#
+#    perform_step([
+#        -deg45, -deg45 - np.pi/6, -deg90 - np.pi/6,
+#        -deg90, 0, +deg45,
+#        -deg45, 0, -deg90,
+#    ])
+#
+#    perform_step([
+#        -deg45, -deg45 - np.pi/6, -deg90 - np.pi/6,
+#        deg45, 0, -deg45,
+#        -deg45, 0, -deg90,
+#    ])
+#
+#    perform_step([
+#        -deg45, -deg45 - np.pi/6, -deg90 - np.pi/6,
+#        -deg90, 0, +deg45,
+#        -deg45, 0, -deg90,
+#    ])
+#
+#    perform_step([
+#        -deg45, -deg45 - np.pi/6, -deg90 - np.pi/6,
+#        -deg90, 0, +deg45,
+#        deg45, 0, -deg45,
+#    ])
+#
+#
+#    while True:
+#        perform_step([
+#            -deg45, -deg45 - np.pi/6, -deg90 - np.pi/6,
+#            -deg90, 0, +deg45,
+#            -deg45, 0, -deg90,
+#        ])
+#
+#        perform_step([
+#            -deg45, -deg45 - np.pi/6, -deg90 - np.pi/6,
+#            deg45, 0, -deg45,
+#            deg45, 0, -deg45,
+#        ])
+#
+
+    perform_step(finger0 + finger120_open + finger240_open)
+    perform_step(finger0 + finger120_open + finger240_clash)
+    perform_step(finger0 + finger120_open + finger240_open)
+    perform_step(finger0 + finger120_clash + finger240_open)
+
+    while True:
+        perform_step(finger0 + finger120_open + finger240_open)
+        perform_step(finger0 + finger120_clash + finger240_clash)
+
 
 def run_choreography(robot):
     """Move the legs in some hard-coded choreography."""
@@ -24,12 +89,14 @@ def run_choreography(robot):
     deg45 = np.pi / 4
 
     while True:
+        # initial pose
         perform_step([
             0, -deg45, -deg45,
             0, -deg45, -deg45,
             0, -deg45, -deg45,
         ])
 
+        # one finger moving to the centre
         perform_step([
             0, +deg45, +deg45,
             0, -deg45, -deg45,
@@ -48,27 +115,29 @@ def run_choreography(robot):
             0, +deg45, +deg45,
         ])
 
+        # initial pose
         perform_step([
             0, -deg45, -deg45,
             0, -deg45, -deg45,
             0, -deg45, -deg45,
         ])
 
+        # side-wards movement
         perform_step([
             -deg45, -deg45, -deg45,
-            0, -deg45, -deg45,
-            0, -deg45, -deg45,
+            0, -deg45, 0,
+            deg45, -deg45, -deg45,
         ])
 
         perform_step([
-            0, -deg45, -deg45,
+            deg45, -deg45, -deg45,
             -deg45, -deg45, -deg45,
-            0, -deg45, -deg45,
+            0, -deg45, 0,
         ])
 
         perform_step([
-            0, -deg45, -deg45,
-            0, -deg45, -deg45,
+            0, -deg45, 0,
+            deg45, -deg45, -deg45,
             -deg45, -deg45, -deg45,
         ])
 
@@ -81,6 +150,7 @@ def main():
 
     # move around
     run_choreography(robot)
+    #run_experimental(robot)
 
 
 if __name__ == "__main__":
